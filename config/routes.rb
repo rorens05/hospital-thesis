c3929eef69d2ffabee7d5b341a3973911230a87e
Laurence Bautista <rorens05@gmail.com>
2019-01-20 11:43:00 +0800

    unsuccessful devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :admins
+  resources :admins
   get 'dashboard/index'
   root 'dashboard#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
